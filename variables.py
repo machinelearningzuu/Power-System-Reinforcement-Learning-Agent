754ebcd2035d33f404e56846f5cbbf7d24478744
NetworkClassificationProject <isurualagiyawanna9717@gmail.com>
2020-09-21 07:49:53 +0530

    # update model hyperparameters

--- variables.py
+++ variables.py
@@ -10,13 +10,13 @@ Dmax = 340
 n_bins = 10
 
 p = 22
-learning_rate = 10e-1
-eps = 0.5
+learning_rate = 1e-3
+eps = 0.7
 discount_factor = 0.99
 num_days = 365
 
-initial_state = (PVmin, Ebmax, 2.5)
-initial_action = 1 # (charging, discharging, Nothing)
+initial_state = (PVmin, Ebmax, Dmin)
+initial_action = 2 # (charging, discharging, Nothing)
 
 demand_csv_path = 'Data/demand.csv'
 data_csv_path = 'Data/solar.csv'
